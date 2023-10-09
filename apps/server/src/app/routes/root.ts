c92ac7d95c471a965db5a36c0fed83b429b02e69
David Gallegos <dgallegos@diligent.com>
2023-10-09 16:46:55 -0300

    Initial commit

---
+++ apps/server/src/app/routes/root.ts
@@ -0,0 +1,17 @@
+import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
+import { Client } from '@audit/interfaces';
+
+export default async function (fastify: FastifyInstance) {
+  const cliente: Client = {
+    first_name: 'as',
+    last_name: 'as',
+    age: 2,
+  };
+
+  fastify.get(
+    '/',
+    async function (request: FastifyRequest, reply: FastifyReply) {
+      return { message: 'Hello API' };
+    }
+  );
+}
